95e427c373040f2ab26616f419748043f732e7f8
Hackbright Student <info@hackbrightacademy.com>
2016-04-06 15:27:22 -0700

    A working while loop, that can add, subtract and multiply

---
+++ calculator.py
@@ -0,0 +1,22 @@
+"""
+calculator.py
+
+Using our arithmetic.py file from Exercise02, create the
+calculator program yourself in this file.
+"""
+
+from arithmetic import *
+
+while True: #starting while loop
+    input = raw_input('> ') #have user enter an operator and two numbers
+    output = input.split(' ') #split will 
+    operator = output[0] #operator will determine what function to use in arithmetic.py
+    num1 = int(output[1]) # input in str, need to convert str to int
+    num2 = int(output[2])
+    
+    if operator == '+':
+        print add(num1, num2)
+    elif operator == '-':
+        print subtract(num1, num2)
+    elif operator == '*':
+        print multiply(num1, num2)
