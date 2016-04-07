3b6891d3fb8d5614f62c0aa643e961b3fe7b5ded
Hackbright Student <info@hackbrightacademy.com>
2016-04-07 12:09:23 -0700

    The add function passes through two numbers for sure.

--- testcalculator.py
+++ testcalculator.py
@@ -8,28 +8,12 @@ calculator program yourself in this file.
 from arithmetic import *
 
 while True: #starting while loop
-     user_nums = raw_input('> ') #have user enter an operator and two numbers
-     user_nums_list = user_nums.split(' ') # create a list and assign 
-     for items in user_nums_list:
-        float(items[1:]) 
+    user_nums = raw_input('> ') #have user enter an operator and two numbers
+    user_nums_list = user_nums.split(' ') # create a list and assign 
+    operator = user_nums_list[0]
     
-
-    # if len(output) == 2:
-    #     operator = output[0] #operator will determine what function to use in arithmetic.py
-    #     try:
-    #         num1 = float(output[1]) # input in str, need to convert str to int
-    #     except:
-    #         print "You did not enter an integer. Please enter an integer."
-    #         continue
-
-
-    # elif len(output) == 3:
-    #     operator = output[0]
-    #     num1 = float(output[1])
-    #     num2 = float(output[2])
-    
-        if operator == '+':
-            print add(user_nums_list[1:])
+    if operator == '+':
+        print add(user_nums_list[1:])
     # elif operator == '-':
     #     print subtract(num1, num2)
     # elif operator == '*':
